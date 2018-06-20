e854e490ca19c8cece986f55645b32e7bf2c9aef
cuemur <dienvidvejs@gmail.com>
2018-06-20 08:47:57 +0300

    forgot this one too...
    -_-

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = ( props ) => {
 	return (
 		<div className='loading'>
-			<img src="http://localhost:3333/static/assets/img/loading.svg" />
+			<img src="http://localhost:3333/static/assets/img/icons/loading.svg" />
 		</div>
 	);
 }
